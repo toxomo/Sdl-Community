f4bf7b03f316850955c619290cdfd962d1db13c4
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-04-02 08:03:46 +0300

    Add dal solution for database

---
+++ SignoffVerifySettings/Sdl.Community.SignoffVerifySettings.DAL/SignoffVerifySettingsConnection.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+
+namespace Sdl.Community.SignoffVerifySettings.DAL
+{
+	public static class SignoffVerifySettingsConnection
+	{
+		public static string _connectionString;
+		static SignoffVerifySettingsConnection()
+		{
+			_connectionString = GetConnectionString();
+		}
+
+		public static string DbConnectionString
+		{
+			get { return _connectionString; }
+		}
+
+		private static string GetConnectionString()
+		{
+			var builder = new DbConnectionStringBuilder();
+
+			// Local path connection string
+			builder["Data Source"] = "cjsqlserver2016.development.sheffield.sdl.corp";
+			builder["integrated security"] = "True";
+			builder["initial catalog"] = "SignoffVerifySettings";
+			builder["MultipleActiveResultSets"] = "True";
+			builder["App"] = "EntityFramework";
+
+			return builder.ConnectionString;
+		}
+	}
+}
---
+++ SignoffVerifySettings/Sdl.Community.SignoffVerifySettings.DAL/SignoffVerifySettingsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Community.SignoffVerifySettings.Model;
+
+namespace Sdl.Community.SignoffVerifySettings.DAL
+{
+	public class SignoffVerifySettingsRepository
+	{
+		
+	}
+}
