98a81798bee3f3e6245860f5bd5d16ad187ebdb2
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-06-18 13:31:58 +0300

    Add solution to repository

---
+++ TmxTmDatabaseManager/TmxTmDatabase/TmxTmDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TmxTmDatabase
+{
+    public class TmxTmDb
+    {
+		private string _url = "mongodb://localhost:27017";
+
+		/// <summary>
+		/// Insert TMX file to MongoDb TmxTmDatabase
+		/// </summary>
+		public void InsertFile(string filePath)
+		{
+			var client = new MongoDB.Driver.MongoClient(_url);
+			// If TmxTmDatabase does not exists, it is created automatically
+			var db = client.GetDatabase("TmxTmDatabase");
+			var mongoGridFS = new MongoGridFS(db);
+
+			//using (var fs = new FileStream(filePath, FileMode.Open))
+			//{
+			//	var gridFsInfo = db.
+			//}
+		}
+
+		/// <summary>
+		/// Remove TMX file from the MongoDb TmxTmDatabase
+		/// </summary>
+		public void RemoveFile(string filePath)
+		{
+			var client = new MongoClient(_url);
+			// If TmxTmDatabase does not exists, it is created automatically
+			var db = client.GetDatabase("TmxTmDatabase");
+		}
+	}
+}
---
+++ TmxTmDatabaseManager/TmxTmDatabaseManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Sdl.Community.BackupService.Helpers;
+using TmxTmDatabase;
+using TmxTmDatabaseManager.Commands;
+
+namespace TmxTmDatabaseManager.ViewModels
+{
+	public class MainViewModel
+	{
+		private ICommand _openDialogCommand;
+		private TmxTmDb _tmxTmDb = new TmxTmDb();
+
+		public MainViewModel()
+		{
+		}
+
+		public ICommand OpenDialogCommand => _openDialogCommand ?? (_openDialogCommand = new CommandHandler(OpenDialog, true));
+
+		private void OpenDialog()
+		{
+			var dlg = new OpenFileDialog();
+			dlg.DefaultExt = ".tmx";
+			dlg.Filter = "TMX Files (*.tmx)|*.tmx";
+			var dialogResult = dlg.ShowDialog();
+
+			if (dialogResult == true)
+			{
+				foreach (var file in dlg.FileNames)
+				{
+					_tmxTmDb.InsertFile(file);
+				}
+			}
+		}
+	}	
+}
