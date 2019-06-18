5c6a3db24d7ad44e515978d2dc304d86cac192b8
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-06-18 15:25:29 +0300

    Add Ui basic

--- TmxTmDatabaseManager/TmxTmDatabaseManager/ViewModels/MainViewModel.cs
+++ TmxTmDatabaseManager/TmxTmDatabaseManager/ViewModels/MainViewModel.cs
@@ -2,33 +2,66 @@
 using Microsoft.Win32;
 using TmxTmDatabase;
 using TmxTmDatabaseManager.Commands;
+using TMXTMDatabaseManager.Model;
 
 namespace TmxTmDatabaseManager.ViewModels
 {
-	public class MainViewModel
+	public class MainViewModel : ViewModelBase
 	{
+		private TmxTmDb _tmxTmDb;
+		private string _selectedFilePaths;
 		private ICommand _openDialogCommand;
-		private TmxTmDb _tmxTmDb = new TmxTmDb();
+		private ICommand _insertFileCommand;
 
 		public MainViewModel()
 		{
+			_tmxTmDb = new TmxTmDb();
 		}
 
 		public ICommand OpenDialogCommand => _openDialogCommand ?? (_openDialogCommand = new CommandHandler(OpenDialog, true));
+		public ICommand InsertFileCommand => _insertFileCommand ?? (_insertFileCommand = new CommandHandler(InsertFile, true));
 
-		private async void OpenDialog()
+		public string SelectedFilePaths
+		{
+			get => _selectedFilePaths;
+			set
+			{
+				_selectedFilePaths = value;
+				OnPropertyChanged(nameof(SelectedFilePaths));
+			}
+		}
+
+		private void OpenDialog()
 		{
 			var dlg = new OpenFileDialog();
 			dlg.DefaultExt = ".tmx";
 			dlg.Filter = "TMX Files (*.tmx)|*.tmx";
+			dlg.Multiselect = true;
 			var dialogResult = dlg.ShowDialog();
 
 			if (dialogResult == true)
 			{
-				foreach (var file in dlg.FileNames)
+				if (dlg.FileNames.Length > 1)
 				{
-					await _tmxTmDb.InsertFile(file);
+					foreach (var file in dlg.FileNames)
+					{
+						SelectedFilePaths += $"{file};";
+					}
+					SelectedFilePaths.TrimEnd(',');
 				}
+				else
+				{
+					SelectedFilePaths = dlg.FileName;
+				}
+			}
+		}
+
+		private async void InsertFile()
+		{
+			var filePaths = SelectedFilePaths.Split(';');
+			foreach(var filePath in filePaths)
+			{
+				await _tmxTmDb.InsertFile(filePath);
 			}
 		}
 	}	
