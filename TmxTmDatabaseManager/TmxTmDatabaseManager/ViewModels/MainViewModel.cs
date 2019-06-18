d2ce148eb971ab720e4592414a9cbb85a4ac49e5
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-06-18 13:38:20 +0300

    Remove unused code

--- TmxTmDatabaseManager/TmxTmDatabaseManager/ViewModels/MainViewModel.cs
+++ TmxTmDatabaseManager/TmxTmDatabaseManager/ViewModels/MainViewModel.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using Microsoft.Win32;
-using Sdl.Community.BackupService.Helpers;
 using TmxTmDatabase;
 using TmxTmDatabaseManager.Commands;
 
